b6363a24e2eb040a5d23ade5079d377f3b3127dd
RafiMoreno <ahmadarrafi.m@gmail.com>
2022-09-12 19:38:02 +0700

    conllu read

--- process.py
+++ process.py
@@ -1,4 +1,5 @@
 from aksara_tokenizer import BaseTokenizer
+from conllu import parse
 import re
 
 
@@ -16,6 +17,8 @@ def read():
     input_csui = []
     with open('id_pud-ud-test.conllu', encoding='utf-8', errors='ignore') as f:
         lines = f.readlines()
+        anno = f.read()
+        sentences = parse(anno)
     for line in lines:
         if re.search("^# text =*", line):
             x = line[9:].strip()
@@ -35,12 +38,22 @@ def read():
             x = line[9:].strip()
             input_csui.append(x)
 
+    
+
     aksara_pud = aksara_tokenize(input_pud)
     aksara_gsd = aksara_tokenize(input_gsd)
     aksara_csui = aksara_tokenize(input_csui)
-    print(aksara_pud[0:5])
+    print(aksara_csui[0:2])
 
+def read_conllu():
+    with open('id_csui-ud-test.conllu',  encoding='utf-8', errors='ignore') as data:
+        annotations = data.read()
+    sentences = parse(annotations)
+    print(sentences[0])
+    print(sentences[0][0])
+    print(sentences[0].metadata['text'])
 
 if __name__ == "__main__":
     # main()
     read()
+    read_conllu()
